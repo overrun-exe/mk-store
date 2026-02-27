d56aae807042c248e6574543e99402bdf96a556f
overrun-exe <74562199+overrun-exe@users.noreply.github.com>
2026-02-27 15:47:04 +0300

    fix for logo

--- frontend/src/shims-vue.d.ts
+++ frontend/src/shims-vue.d.ts
@@ -5,6 +5,36 @@ declare module '*.vue' {
   export default component
 }
 
+declare module '*.png' {
+  const src: string
+  export default src
+}
+
+declare module '*.jpg' {
+  const src: string
+  export default src
+}
+
+declare module '*.jpeg' {
+  const src: string
+  export default src
+}
+
+declare module '*.gif' {
+  const src: string
+  export default src
+}
+
+declare module '*.svg' {
+  const src: string
+  export default src
+}
+
+declare module '*.webp' {
+  const src: string
+  export default src
+}
+
 import 'vue-router'
 import { Store } from 'vuex'
 
